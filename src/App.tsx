5d14322ae9da7c79cdbcf4d66ddd9ba566b504ba
František Šilhán <frantisek@slhn.cz>
2024-01-08 15:43:04 +0100

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -25,22 +25,61 @@ const initialToDos: Array<ITodo> =  [
   }
 ];
 
-const reducer = () => {
+type Action = {
+  type: "COMPLETE";
+  id: string;
+} | {
+  type: "DELETE";
+  id: string;
+} | {
+  type: "ADD";
+  title: string;
+};
 
+const reducer = (state: Array<ITodo>, action: Action) => {
+  switch (action.type) {
+    case "COMPLETE":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return {...todo, completed: !todo.completed};
+        }
+        return todo;
+      });
+    case "DELETE":
+      return state.filter((todo) => {
+        return todo.id !== action.id;
+      });
+    case "ADD":
+      break;
+    default:
+      return state;
+  }
+  return state;
 };
 
 function App() {
   //const [todos, dispatch] = useState(initialToDos);
   const [todos, dispatch] = useReducer(reducer, initialToDos);
-
+  const handleComplete = (id: string) => {
+    dispatch({
+      type: "COMPLETE",
+      id: id
+    });
+  };
+  const handleDelete = (id: string) => {
+    dispatch({
+      type: "DELETE",
+      id: id
+    });
+  };
 
   return (
     <>
       {todos.map((todo) => (
         <div>
-          <input type="checkbox" checked={todo.completed} />
+          <input type="checkbox" checked={todo.completed} onChange={() => {handleComplete(todo.id)}} />
           <span>{todo.title}</span>
-          <button>Delete</button>
+          <button onClick={() => {handleDelete(todo.id)}}>Delete</button>
         </div>
       ))}
     </>
